4d6b4c45264604b131f0dc913987c30f3a6d9548
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-04-22 00:46:36 +0100

    Update hex.c

--- hex.c
+++ hex.c
@@ -10,7 +10,7 @@
 void chex(int n)
 {
 	int i = 1, j, temp;
-	char *hex[50];
+	char hex[50];
 
 	while (n != 0)
 	{
@@ -19,6 +19,34 @@ void chex(int n)
 			temp = temp + 48;
 		else
 			temp = temp + 55;
+		hex[i++] = temp;
+		n = n / 16;
+	}
+	j = i - 1;
+	while (j > 0)
+	{
+		_putchar(hex[j]);
+		j--;
+	}
+}
+/**
+ * Chex - converts decimal to hexadecimal and prints it
+ * @n: number to be converted
+ *
+ * Return: nothing
+ */
+void Chex(int n)
+{
+	int i = 1, j, temp;
+	char hex[50];
+
+	while (n != 0)
+	{
+		temp = n % 16;
+		if (temp < 10)
+			temp = temp + 48;
+		else
+			temp = temp + 87;
 		hex[i++] = temp;
 		n = n / 16;
 	}
